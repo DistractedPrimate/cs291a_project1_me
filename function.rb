96a485963a952f9166a23f52c1d7df82202dbe07
DistractedPrimate <m_medearis@ucsb.edu>
2022-10-04 15:38:20 -0700

    i did it

--- function.rb
+++ function.rb
@@ -4,10 +4,81 @@ require 'json'
 require 'jwt'
 require 'pp'
 
+def valid_json?(string)
+  !!(JSON.parse(string)) rescue false
+end
+
 def main(event:, context:)
   # You shouldn't need to use context, but its fields are explained here:
   # https://docs.aws.amazon.com/lambda/latest/dg/ruby-context.html
-  response(body: event, status: 200)
+
+  httpMethod = event['httpMethod']
+
+  # get method
+  if event['path'] == '/'
+    if httpMethod != 'GET'
+      return response(body: event, status: 405)
+    end
+
+    # Responds 403 if a proper Authorization: Bearer <TOKEN> header is not provided.
+    # Responds 401 if either the token is not yet valid, or if it is expired.
+    if !event.key?("headers")
+      return response(body: nil, status: 403)
+    end
+    if !event["headers"].key?("Authorization")
+      return response(body: nil, status: 403)
+    end
+
+    auth_array = event["headers"]["Authorization"].split(" ")
+    if auth_array[0] != "Bearer"
+      return response(body: nil, status: 403)
+    end
+
+    begin
+      # add leeway to ensure the token is valid
+      decoded_token = JWT.decode auth_array[1], ENV['JWT_SECRET'], true, {algorithm: 'HS256' }
+      rescue JWT::ImmatureSignature => e
+        return response(body: e, status: 401)
+      rescue JWT::ExpiredSignature => e
+        return response(body: e, status: 401)
+      rescue JWT::DecodeError => e
+        return response(body: e, status: 403)
+    end
+    
+    decoded_value = decoded_token[0]['data']
+    return response(body: decoded_value, status: 200)
+  
+  elsif event['path'] == '/token'
+    if httpMethod != 'POST'
+      return response(body: event, status: 405)
+    end
+
+    # Responds 415 if the request content type is not application/json.
+    # Responds 422 if the body of the request is not actually json.
+    
+    if event["body"] == nil
+      return response(body: nil, status: 422)
+    end
+
+    if !valid_json?(event["body"])
+      return response(body: nil, status: 422)
+    end
+
+    if event.key?("Headers") and event["Headers"].key?("Content-Type") and event["Headers"]["Content-Type"] != 'application/json'
+      return response(body: nil, status: 415)
+    end
+    
+    jsonData = JSON.parse(event["body"])
+    payload = {
+      data: jsonData,
+      exp: Time.now.to_i + 5,
+      nbf: Time.now.to_i + 2
+    }
+
+    encoded_token = JWT.encode payload, ENV['JWT_SECRET'], 'HS256'
+
+    return response(body: {"token": encoded_token}, status: 201)
+  end
 end
 
 def response(body: nil, status: 200)
